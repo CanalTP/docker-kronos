02e1b34034863c1d0997f3a1452c5fc44a317cb1
pthegner <pierre.thegner@canaltp.fr>
2019-09-10 15:44:06 +0200

    Add python 3.7 for worker

--- worker/build.sh
+++ worker/build.sh
@@ -4,6 +4,8 @@ OPTION=$1
 
 if  [[ $OPTION = "-debian8" ]]; then
     docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker:debian8 -f Dockerfile.debian8 .
+else if [[ $OPTION = "-python3_7" ]]; then
+    docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker:python3_7 -f Dockerfile.python3_7 .
 else
     docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker .
 fi
