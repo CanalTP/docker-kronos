779ec87853254180c53640810706c3925fb24fe1
pthegner <pierre.thegner@canaltp.fr>
2019-10-11 11:45:50 +0200

    Update worker/build.sh
    Co-Authored-By: LAAJIMI Fahmi <fahmi.laajimi@canaltp.fr>

--- worker/build.sh
+++ worker/build.sh
@@ -4,7 +4,7 @@ OPTION=$1
 
 if  [[ $OPTION = "-debian8" ]]; then
     docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker:debian8 -f Dockerfile.debian8 .
-else if [[ $OPTION = "-python3" ]]; then
+elif [[ $OPTION = "-python3" ]]; then
     docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker:python3_7 -f Dockerfile.python3_7 .
 else
     docker build -t par-vm232.srv.canaltp.fr:5000/kronos-worker .
